015bd57b6bc803909da1e961ea6bc2fbd6dd30c8
imon018 <imon5439@gmail.com>
2026-07-06 00:35:46 +0600

    OrderSuccess.jsx

---
+++ src/pages/OrderSuccess.jsx
@@ -0,0 +1,15 @@
+export default function OrderSuccess() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+
+      <h1 className="text-5xl font-bold text-green-600">
+        Order Successful
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        Thank you for your purchase.
+      </p>
+
+    </div>
+  );
+}
