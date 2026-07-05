1c2199b8f06e17df23e27b660e44bf1a327ebdd6
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:07:57 +0600

    Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import useCart from "../hooks/useCart";
+
+export default function Navbar() {
+  const { cart } = useCart();
+
+  return (
+    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+
+      <Link to="/" className="text-2xl font-bold text-primary">
+        Dream Mode
+      </Link>
+
+      <div className="flex gap-6 items-center">
+
+        <Link to="/shop">Shop</Link>
+        <Link to="/wishlist">Wishlist</Link>
+        <Link to="/cart">
+          Cart ({cart.length})
+        </Link>
+
+        <Link to="/login">Login</Link>
+
+      </div>
+
+    </nav>
+  );
+}
