d6c6cfa5ea1525a2175cefdb044441000ec8545c
Imon Ahmed <imon5439@gmail.com>
2026-07-06 03:05:46 +0600

    errorBoundary.js

---
+++ src/utils/errorBoundary.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return this.props.children;
+  }
+}
