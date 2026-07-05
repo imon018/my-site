c5997606623c132707bb51de498efd4e5c0803a8
Imon Ahmed <imon5439@gmail.com>
2026-07-06 05:14:43 +0600

    siteConfig.js

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -1,11 +1,13 @@
 export const siteConfig = {
-  appName: import.meta.env.VITE_APP_NAME,
+  appName: "Dream Mode",
 
-  email: import.meta.env.VITE_BUSINESS_EMAIL,
+  email: "dreammode27@gmail.com",
 
-  phone: import.meta.env.VITE_BUSINESS_PHONE,
+  phone: "+8801406978619",
 
-  whatsapp: `https://wa.me/${import.meta.env.VITE_WHATSAPP_NUMBER}?text=Hello%20Dream%20Mode,%20I'm%20interested%20in%20your%20products.`,
+  whatsapp:
+    "https://wa.me/8801406978619?text=Hello%20Dream%20Mode,%20I'm%20interested%20in%20your%20products.",
 
-  facebook: import.meta.env.VITE_FACEBOOK_URL,
+  facebook:
+    "https://www.facebook.com/profile.php?id=61560486613695",
 };
