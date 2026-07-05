b1d022bf013ee41e38e249e757ef9719c56ba070
imon018 <imon5439@gmail.com>
2026-07-06 00:25:35 +0600

    ProtectedRoute.jsx

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({
+  children,
+  isAuthenticated = false
+}) {
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+
+}
