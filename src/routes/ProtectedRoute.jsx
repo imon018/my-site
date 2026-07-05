52df342203187eb252523f88f03bd55aaa9e8760
Imon Ahmed <imon5439@gmail.com>
2026-07-06 01:16:26 +0600

    ProtectedRoute.jsx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,14 +1,15 @@
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Spinner from "../components/Spinner";
 
-export default function ProtectedRoute({
-  children,
-  isAuthenticated = false
-}) {
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
 
-  if (!isAuthenticated) {
+  if (loading) return <Spinner />;
+
+  if (!user) {
     return <Navigate to="/login" replace />;
   }
 
   return children;
-
 }
