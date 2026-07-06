b7318906ad51129380c83630f144d9421b252e80
Imon Ahmed <imon5439@gmail.com>
2026-07-06 16:48:24 +0600

    AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 
+import Home from "../pages/Home";
+
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={<h1 style={{ padding: "40px" }}>Route Working ✅</h1>}
-      />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
