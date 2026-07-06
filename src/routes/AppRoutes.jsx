e3e15b485a79c99fd2d081834eb4dbb98f444ec8
Imon Ahmed <imon5439@gmail.com>
2026-07-06 16:40:46 +0600

    AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,40 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
-import MainLayout from "../layouts/MainLayout";
-
-import Home from "../pages/Home";
-import Shop from "../pages/Shop";
-import Cart from "../pages/Cart";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import ProductDetails from "../pages/ProductDetails";
-import Checkout from "../pages/Checkout";
-import Wishlist from "../pages/Wishlist";
-import Profile from "../pages/Profile";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import OrderSuccess from "../pages/OrderSuccess";
-import NotFound from "../pages/NotFound";
-
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/order-success" element={<OrderSuccess />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
+      <Route
+        path="/"
+        element={<h1 style={{ padding: "40px" }}>Route Working ✅</h1>}
+      />
     </Routes>
   );
 }
