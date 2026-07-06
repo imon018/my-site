003fcdd0ed594508cf8d44f2f68f4279fff5b742
Imon Ahmed <imon5439@gmail.com>
2026-07-06 07:04:06 +0600

    Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import { siteConfig } from "../config/siteConfig";
-default function Contact() {
+
+export default function Contact() {
   return (
     <div className="max-w-4xl mx-auto px-6 py-12">
 
