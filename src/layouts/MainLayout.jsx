be507887ce6368dc95128385528bbb6b4898893e
Imon Ahmed <imon5439@gmail.com>
2026-07-09 18:09:02 +0600

    Add MobileBottomNav and adjust main layout padding

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import MobileBottomNav from "../components/MobileBottomNav";
 
 export default function MainLayout() {
   return (
     <>
       <Header />
 
-      <main className="min-h-screen">
+      <main className="min-h-screen pb-20 md:pb-0">
         <Outlet />
       </main>
+
+      <MobileBottomNav />
 
       <Footer />
     </>
