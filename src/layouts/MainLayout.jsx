6a8611e9dea0a0e00fb2fabe7cf4500d199ff436
Imon Ahmed <imon5439@gmail.com>
2026-07-10 12:46:11 +0600

    Add AnnouncementBar component to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 
+import AnnouncementBar from "../components/AnnouncementBar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MobileBottomNav from "../components/MobileBottomNav";
@@ -8,6 +9,8 @@ import WhatsAppButton from "../components/WhatsAppButton";
 export default function MainLayout() {
   return (
     <>
+      <AnnouncementBar />
+
       <Header />
 
       <main className="min-h-screen pb-20 md:pb-0">
