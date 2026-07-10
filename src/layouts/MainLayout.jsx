006cfbea68215549110eddbfccea78683d6a77ac
Imon Ahmed <imon5439@gmail.com>
2026-07-10 12:31:00 +0600

    Integrate WhatsAppButton into MainLayout
    Added WhatsAppButton component to MainLayout.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MobileBottomNav from "../components/MobileBottomNav";
+import WhatsAppButton from "../components/WhatsAppButton";
 
 export default function MainLayout() {
   return (
@@ -12,6 +13,8 @@ export default function MainLayout() {
       <main className="min-h-screen pb-20 md:pb-0">
         <Outlet />
       </main>
+
+      <WhatsAppButton />
 
       <MobileBottomNav />
 
