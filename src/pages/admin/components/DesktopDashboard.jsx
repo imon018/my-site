d807584dabf6488394769aa305c0c954d5f7a785
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-12 15:55:30 +0600

    Register Chart.js components in DesktopDashboard
    Register Chart.js components for use in DesktopDashboard.

--- src/pages/admin/components/DesktopDashboard.jsx
+++ src/pages/admin/components/DesktopDashboard.jsx
@@ -14,6 +14,29 @@ import {
 } from "react-chartjs-2";
 
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Tooltip,
+  Legend
+} from "chart.js";
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Tooltip,
+  Legend
+);
+
+
 
 export default function DesktopDashboard({data}){
 
