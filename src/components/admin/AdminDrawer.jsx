c5e80659c0a89209d4e9df6a947bbaaf65230afd
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-15 20:43:26 +0600

    Add AdminDrawer component for admin navigation

---
+++ src/components/admin/AdminDrawer.jsx
@@ -0,0 +1,154 @@
+import {
+  useNavigate,
+} from "react-router-dom";
+
+
+import AdminDrawerHeader from "./AdminDrawerHeader";
+
+import AdminDrawerMenu from "./AdminDrawerMenu";
+
+
+
+
+
+export default function AdminDrawer({
+
+  open,
+
+  setOpen,
+
+}) {
+
+
+const navigate = useNavigate();
+
+
+
+
+
+const closeDrawer = ()=>{
+
+  setOpen(false);
+
+};
+
+
+
+
+
+
+return (
+
+<>
+
+
+{/* OVERLAY */}
+
+
+{
+open &&
+
+<div
+
+className="
+fixed
+inset-0
+bg-black/40
+backdrop-blur-sm
+z-[60]
+lg:hidden
+"
+
+onClick={closeDrawer}
+
+></div>
+
+}
+
+
+
+
+
+
+{/* DRAWER */}
+
+
+<div
+
+className={`
+fixed
+top-0
+left-0
+h-screen
+w-[320px]
+bg-white
+z-[70]
+shadow-2xl
+transition-transform
+duration-300
+flex
+flex-col
+
+${
+open
+?
+"translate-x-0"
+:
+"-translate-x-full"
+}
+
+`}
+
+>
+
+
+
+
+
+
+<AdminDrawerHeader
+
+
+closeDrawer={closeDrawer}
+
+
+onNotificationClick={()=>{
+
+navigate("/admin/notifications");
+
+closeDrawer();
+
+}}
+
+
+/>
+
+
+
+
+
+
+<AdminDrawerMenu
+
+
+closeDrawer={closeDrawer}
+
+
+/>
+
+
+
+
+
+</div>
+
+
+
+
+
+</>
+
+);
+
+
+}
