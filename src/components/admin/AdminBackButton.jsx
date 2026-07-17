35a4a63074b3b9ba904353141b721470d4505031
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-17 13:58:52 +0600

    Add drawer toggle functionality to AdminBackButton

--- src/components/admin/AdminBackButton.jsx
+++ src/components/admin/AdminBackButton.jsx
@@ -1,22 +1,96 @@
 import {
- FiArrowLeft
+  FiArrowLeft,
+  FiMenu,
 } from "react-icons/fi";
 
+
 import {
- useNavigate
+  useLocation,
+  useNavigate,
 } from "react-router-dom";
 
 
-export default function AdminBackButton(){
+
+
+export default function AdminBackButton({
+
+setDrawerOpen
+
+}){
+
 
 const navigate = useNavigate();
+
+const location = useLocation();
+
+
+
+
+
+// MAIN ADMIN PAGES
+
+const mainRoutes = [
+
+"/admin",
+
+"/admin/profile",
+
+"/admin/users",
+
+"/admin/products",
+
+"/admin/orders",
+
+"/admin/return-orders",
+
+"/admin/send-notification",
+
+"/admin/settings",
+
+"/admin/subscribers",
+
+];
+
+
+
+
+
+const isMainPage =
+mainRoutes.includes(
+location.pathname
+);
+
+
+
+
 
 
 return (
 
 <button
 
-onClick={()=>navigate(-1)}
+
+onClick={()=>{
+
+
+if(isMainPage){
+
+setDrawerOpen(true);
+
+
+}
+
+else{
+
+
+navigate(-1);
+
+
+}
+
+
+}}
+
 
 className="
 mb-4
@@ -28,14 +102,45 @@ font-medium
 hover:opacity-70
 "
 
+
 >
+
+
+
+{
+
+isMainPage
+
+?
+
+<>
+
+<FiMenu size={24}/>
+
+Menu
+
+</>
+
+
+:
+
+<>
 
 <FiArrowLeft size={22}/>
 
 Back
 
+</>
+
+
+}
+
+
+
 </button>
 
+
 );
+
 
 }
