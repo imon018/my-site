7876afe94465a5e64543529518a90406b44b6fcd
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-20 05:23:20 +0600

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import Hero from "../components/Hero";
+import FeaturedProducts from "../components/FeaturedProducts";
+import WhyChooseUs from "../components/WhyChooseUs";
+import StatsSection from "../components/StatsSection";
+import TrustBanner from "../components/TrustBanner";
+import AnimatedWrapper from "../components/ui/AnimatedWrapper";
+import PremiumHighlights from "../components/PremiumHighlights";
+import Newsletter from "../components/Newsletter";
+
+export default function Home() {
+  
+  return (
+  <div className="bg-[#FAF7F2]">
+    <AnimatedWrapper>
+
+      <Hero />
+
+      <PremiumHighlights />
+
+      <FeaturedProducts />
+
+      <WhyChooseUs />
+
+      <StatsSection />
+
+      <TrustBanner />
+
+      <Newsletter />
+
+    </AnimatedWrapper>
+  </div>
+);
+}
