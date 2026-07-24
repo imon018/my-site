017a03c676c5f28fb7d71c942c350780ea994e1e
Imon0180 <35893613+Imon0180@users.noreply.github.com>
2026-07-24 21:22:31 +0600

    Import SettingsProvider from context

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import { SettingsProvider } from "./context/SettingsContext";
 
 import App from "./App";
 import "./index.css";
